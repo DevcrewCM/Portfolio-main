640c2585b1430df6a50e8a37a5484f402f09e8de
Cristian Meneses <ingcristianmen@gmail.com>
2022-12-06 09:48:34 -0500

    first commit

---
+++ scripts/writing.js
@@ -0,0 +1,14 @@
+export default function writeEffect(element) {
+  const typed = new Typed(element, {
+    strings: [" Cristian Meneses"],
+    typeSpeed: 85,
+    startDelay: 350,
+    backSpeed: 85,
+    smartBackspace: true,
+    backDelay: 1000,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    contentType: "html",
+  });
+}
